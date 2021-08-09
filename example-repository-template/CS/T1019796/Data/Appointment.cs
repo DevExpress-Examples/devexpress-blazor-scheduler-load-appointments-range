730b01410cc06d13f19c9b21262718100383000c
CodeKarno <ekaterina.khlopova@devexpress.com>
2021-08-09 11:47:47 +0300

    first commit

---
+++ example-repository-template/CS/T1019796/Data/Appointment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace T1019796.Data
+{
+    public class Appointment
+    {
+        public Appointment() { }
+        
+        public int AppointmentId { get; set; }
+        public int AppointmentType { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Caption { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public int? Label { get; set; }
+        public int Status { get; set; }
+        public bool AllDay { get; set; }
+        public string Recurrence { get; set; }
+        public int? ResourceId { get; set; }
+        public bool Accepted { get; set; }
+    }
+}
---
+++ example-repository-template/CS/T1019796/Data/AppointmentCollection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace T1019796.Data
+{
+    public static partial class AppointmentCollection
+    {
+
+        public static IEnumerable<Appointment> GetAppointments(DateTime startDate, DateTime endDate)
+        {
+            var result = GenerateAppointments().Where(p => p.StartDate >= startDate && p.EndDate <= endDate);
+            return result;
+        }
+
+        private static List<Appointment> GenerateAppointments()
+        {
+            DateTime date = DateTime.Now.Date;
+            var dataSource = new List<Appointment>();
+            for (int i = 0; i < 100; i++)
+            {
+                Appointment appt = new Appointment
+                {
+                    AppointmentId = i,
+                    Caption = $"Appointment{i}",
+                    Status = i % 2,
+                    Label = i % 2,
+                    StartDate = date + new TimeSpan(i, i % 24, i % 60, i % 60)
+                };
+                appt.EndDate = appt.StartDate.AddHours(3);
+                dataSource.Add(appt);
+            }
+            return dataSource;
+        }
+    }
+}
+
