7414adc89d097b58a8d3ca1376d52865d89db60b
Elena Khamlyuk <elena.khamlyuk@devexpress.com>
2022-06-13 14:29:02 +0300

    upd

--- CS/T1019796/Data/Appointment.cs
+++ CS/T1019796/Data/Appointment.cs
@@ -8,18 +8,17 @@ namespace T1019796.Data
     public class Appointment
     {
         public Appointment() { }
-        
         public int AppointmentId { get; set; }
         public int AppointmentType { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public string Caption { get; set; }
-        public string Description { get; set; }
-        public string Location { get; set; }
+        public string? Caption { get; set; }
+        public string? Description { get; set; }
+        public string? Location { get; set; }
         public int? Label { get; set; }
         public int Status { get; set; }
         public bool AllDay { get; set; }
-        public string Recurrence { get; set; }
+        public string? Recurrence { get; set; }
         public int? ResourceId { get; set; }
         public bool Accepted { get; set; }
     }
--- CS/T1019796/Program.cs
+++ CS/T1019796/Program.cs
@@ -1,21 +1,37 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using T1019796.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
-namespace T1019796
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddDevExpressBlazor();
+builder.Services.Configure<DevExpress.Blazor.Configuration.GlobalOptions>(options => {
+    options.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
+});
+builder.WebHost.UseWebRoot("wwwroot");
+builder.WebHost.UseStaticWebAssets();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStaticWebAssets();
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
